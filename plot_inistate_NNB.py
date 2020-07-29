10d49e8953d407a757a6fb275d67d2a85f9d612b
matval <matteo.vallar@epfl.ch>
2020-07-29 09:09:24 +0200

    Added plot of rz

--- plot_inistate_NNB.py
+++ plot_inistate_NNB.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 sys.path.append('/home/matval/WORK/pythonscripts')
 import utils.plot_utils as pu
 import a5py.ascot5io.ascot5 as a5
@@ -6,22 +6,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import a5py.marker.evaluate as evaluate
 pu.common_style()
-
-dir='/home/matval/'
-dir+='/WORK/ASCOT/runs/SA_003/2D/nnb/orbits';
+dir='/home/vallar/'
+if os.uname().nodename!='spcpc182':
+    dir='/home/matval/'
+dir+='/WORK/ASCOT/runs/SA_003/2D/nnb/orbits'
 a=a5.Ascot(f'{dir}/ascot.h5')
 run=a.run_1593691660
 #B field
 #b2d = a.bfield.B_2DS_0346916261.read()
 b2d=run.bfield.read()
-# R_bfield = np.linspace(b2d['rmin'][0], b2d['rmax'][0], b2d['nr'][0])
-# z_bfield = np.linspace(b2d['zmin'][0], b2d['zmax'][0], b2d['nz'][0])
-# psi_norm = (b2d['psi']-b2d['psi0'])/(b2d['psi1']-b2d['psi0'])
-# rho_pol = np.sqrt(psi_norm)
-# #wall (better use 2D)
-# wall=a.wall.wall_2D_3087769866
-# wall=wall.read()
-#wall=a.active.wall
+R_bfield = np.linspace(b2d['rmin'][0], b2d['rmax'][0], b2d['nr'][0])
+z_bfield = np.linspace(b2d['zmin'][0], b2d['zmax'][0], b2d['nz'][0])
+psi_norm = (b2d['psi']-b2d['psi0'])/(b2d['psi1']-b2d['psi0'])
+rho_pol = np.sqrt(psi_norm)
+#wall (better use 2D)
+wall=a.wall.wall_2D_3087769866
+wall=wall.read()
 
 #particles
 inistate=run.inistate.read()
@@ -31,9 +31,10 @@ pitch=evaluate.eval_particle('pitch', mass=inistate['mass'], charge=None,
                   BR=inistate['br'], Bphi=inistate['bphi'], Bz=inistate['bz'], psi=None)
 #Trapping condition
 d = np.sqrt((b2d['axisr']-inistate['r'])**2+(b2d['axisz']-inistate['z'])**2)
-lhs = pitch/sqrt(1-pitch**2)
-rhs = sqrt(d*2./abs(b2d['axisr']-d))
+lhs = pitch/np.sqrt(1-pitch**2)
+rhs = np.sqrt(d*2./abs(b2d['axisr']-d))
 ind_trapp = np.where(lhs<rhs)
+
 #rhopitch
 f=plt.figure(figsize=(6,10));
 ax=f.add_subplot(111);
@@ -42,5 +43,18 @@ ax.scatter(inistate['rho'][lhs<rhs], pitch[lhs<rhs], color='r', label='Trapped')
 ax.legend(loc='best');ax.set_xlabel(r'$\rho_{pol}$')
 ax.set_ylabel(r'$\frac{v_{||}}{v}$')
 ax.grid('on')
+f.tight_layout()
+plt.show()
+
+#rz
+f=plt.figure(figsize=(6,10));
+ax=f.add_subplot(111);
+ax.contour(R_bfield, z_bfield, rho_pol.T, colors='k')
+ax.plot(wall['r'], wall['z'], 'k', lw=3)
+ax.scatter(inistate['r'][lhs>rhs], inistate['z'][lhs>rhs], color='k',label='Passing') #PASSING
+ax.scatter(inistate['r'][lhs<rhs], inistate['z'][lhs<rhs], color='r', label='Trapped') #TRAPPED
+ax.legend(loc='best');ax.set_xlabel(r'$R [m]$')
+ax.set_ylabel(r'$z [m]$')
+ax.grid('on'); ax.axis('equal')
 f.tight_layout()
 plt.show()
